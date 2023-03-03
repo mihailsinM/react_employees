fe74c6e5b178cf9a0edfd2f1c70c02334b88b30e
mihailsinM <mihailsin49@gmail.com>
2023-03-03 15:08:02 +0200

    Criete project

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+import './app-info.css'
+
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Accounting of employees in company SINM</h1>
+            <h2>Number of employees: </h2>
+            <h2>Bonus will receive: </h2>
+
+        </div>
+    )
+}
+export default AppInfo;
