1e83e0eedea4094883c07ada4b6020da2a416fd2
mihailsinM <mihailsin49@gmail.com>
2023-04-11 21:18:07 +0300

    added sass module

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
-import './employees-add-form.css';
+// import './employees-add-form.css';
+import './employees-add-form.scss';
 
 class EmployeesAddForm extends Component {
     constructor(props) {
@@ -37,7 +38,7 @@ class EmployeesAddForm extends Component {
                 <form
                     className="add-form d-flex"
                     onSubmit={this.onSubmit}>
-                        
+
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="What's his name?"
